543f2ed08e88ad16a0714543c6b951695530bdfe
David <davidbalyan9@gmail.com>
2021-03-12 20:14:17 +0200

    Add everything

--- script.js
+++ script.js
@@ -1,46 +1,118 @@
 let rock = document.getElementById("rock");
-let paper = document.getElementById("paper");
-let scissors = document.getElementById("scissors");
-
 let rockPC = document.getElementById("rockPC");
+let paper = document.getElementById("paper");
 let paperPC = document.getElementById("paperPC");
+let scissors = document.getElementById("scissors");
 let scissorsPC = document.getElementById("scissorsPC");
-
-let result = document.getElementById("result");
 
 let value;
 let valuePC;
+let prevValuePC;
 
-function getAndCompareValues() {
+let result = document.getElementById("result");
+
+function main() {
+    //when user chooses rock
     rock.onclick = () => {
-        value = 0;
         rock.style.backgroundColor = "#d17702";
-        paper.style.backgroundColor = "white";
-        scissors.style.backgroundColor = "white";
-        valuePC = Math.floor(Math.random() * Math.floor(3));
-    };
 
+        value = 0;
+
+        //get pc value
+        valuePC = Math.floor(Math.random() * Math.floor(3));
+
+        //change rockpc background
+        if (valuePC == 0) {
+            rockPC.style.backgroundColor = "#d17702";
+            paperPC.style.backgroundColor = "white";
+            scissorsPC.style.backgroundColor = "white";
+        } else if (valuePC == 1) {
+            rockPC.style.backgroundColor = "white";
+            paperPC.style.backgroundColor = "#d17702";
+            scissorsPC.style.backgroundColor = "white";
+        } else {
+            rockPC.style.backgroundColor = "white";
+            paperPC.style.backgroundColor = "white";
+            scissorsPC.style.backgroundColor = "#d17702";
+        }
+
+        //change result text
+        if (value == valuePC) {
+            result.innerHTML = "You Draw";
+        } else if (valuePC == 2) {
+            result.innerHTML = "You Win";
+        } else {
+            result.innerHTML = "You Lose";
+        }
+    }
+
+    //when user chooses paper
     paper.onclick = () => {
-        value = 1;
-        rock.style.backgroundColor = "white";
         paper.style.backgroundColor = "#d17702";
-        scissors.style.backgroundColor = "white";
-        valuePC = Math.floor(Math.random() * Math.floor(3));
-    };
 
+        value = 1;
+
+        //get pc value
+        valuePC = Math.floor(Math.random() * Math.floor(3));
+
+        //change rockpc background
+        if (valuePC == 0) {
+            rockPC.style.backgroundColor = "#d17702";
+            paperPC.style.backgroundColor = "white";
+            scissorsPC.style.backgroundColor = "white";
+        } else if (valuePC == 1) {
+            rockPC.style.backgroundColor = "white";
+            paperPC.style.backgroundColor = "#d17702";
+            scissorsPC.style.backgroundColor = "white";
+        } else {
+            rockPC.style.backgroundColor = "white";
+            paperPC.style.backgroundColor = "white";
+            scissorsPC.style.backgroundColor = "#d17702";
+        }
+
+        //change result text
+        if (value == valuePC) {
+            result.innerHTML = "You Draw";
+        } else if (valuePC == 1) {
+            result.innerHTML = "You Win";
+        } else {
+            result.innerHTML = "You Lose";
+        }
+    }
+
+    //when user chooses scissors
     scissors.onclick = () => {
-        value = 2;
-        rock.style.backgroundColor = "white";
-        paper.style.backgroundColor = "white";
         scissors.style.backgroundColor = "#d17702";
-        valuePC = Math.floor(Math.random() * Math.floor(3));
-    };
 
-    if (value == valuePC) {
-        result.innerHTML = "You Draw";
-    } else {
-        result.innerHTML = "eh";
+        value = 2;
+
+        //get pc value
+        valuePC = Math.floor(Math.random() * Math.floor(3));
+
+        //change rockpc background
+        if (valuePC == 0) {
+            rockPC.style.backgroundColor = "#d17702";
+            paperPC.style.backgroundColor = "white";
+            scissorsPC.style.backgroundColor = "white";
+        } else if (valuePC == 1) {
+            rockPC.style.backgroundColor = "white";
+            paperPC.style.backgroundColor = "#d17702";
+            scissorsPC.style.backgroundColor = "white";
+        } else {
+            rockPC.style.backgroundColor = "white";
+            paperPC.style.backgroundColor = "white";
+            scissorsPC.style.backgroundColor = "#d17702";
+        }
+
+        //change result text
+        if (value == valuePC) {
+            result.innerHTML = "You Draw";
+        } else if (valuePC == 1) {
+            result.innerHTML = "You Win";
+        } else {
+            result.innerHTML = "You Lose";
+        }
     }
 }
 
-getAndCompareValues();
+main();
